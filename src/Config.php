54b8d2469ef281074c5a556c0c30a6697164577b
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-04-25 18:06:01 +0100

    config: Add explicit comments on keys

--- src/Config.php
+++ src/Config.php
@@ -3,16 +3,37 @@
 namespace SpringDvs;
 
 class Config {
+	/**
+	 * Node configuration
+	 * 
+	 * Keys:
+	 * 
+	 * - hostname => Node's hostname
+	 * - springname => Node's springname
+	 * - testing => Toggle whether node is in testing mode
+	 * 
+	 */
 	public static $spec = array(
 		'hostname' => "Default",
 		'springname' => 'Dft',
 		'testing' => false,
 	);
 	
+	/**
+	 * Network configuration
+	 * 
+	 * Keys:
+	 * 
+	 * - master => Master root address
+	 * - hostname => Hostname of the root node
+	 * - service => The service protocol of the root node
+	 * - geosub => The geosub the node is connected to
+	 */
 	public static $net = array (
 		'master' => "127.0.0.1",
 		'hostname' => "default.hst",
 		'service' => "http",
+		'geosub' => "gsndft",
 	);
 	
 };
