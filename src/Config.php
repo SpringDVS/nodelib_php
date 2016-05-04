78d6e529eeddfaf2c56ff8f753d18d86950c32ae
Charlie Fyvie-Gauld <cfg@zunautica.org>
2016-05-04 23:34:25 +0100

    config: Add handing for master root host resource

--- src/Config.php
+++ src/Config.php
@@ -26,6 +26,7 @@ class Config {
 	 * 
 	 * - master => Master root address
 	 * - hostname => Hostname of the root node
+	 * - hostres => Hostname service resource
 	 * - service => The service protocol of the root node
 	 * - geosub => The geosub the node is connected to
 	 * - geotop => The geotop the node GSN is a part of
@@ -33,6 +34,7 @@ class Config {
 	public static $net = array (
 		'master' => "127.0.0.1",
 		'hostname' => "default.hst",
+		'hostres' => "res",
 		'service' => "http",
 		'geosub' => "gsndft",
 		'geotop' => "gtndft,"
@@ -47,4 +49,8 @@ function nodereg_from_config() {
 
 function nodeurl_from_config() {
 	return Config::$spec['springname'] . "." . Config::$net['geosub'] . "." . Config::$net['geotop'];
+}
+
+function hostres_from_config() {
+	return Config::$net['hostname'] . "/" . Config::$net['hostres'];
 }
